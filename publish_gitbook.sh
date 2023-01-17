3fcaebd394d9e8e61831b1addcf3f1c6303c36c6
Murray Jason <mrj@acm.org>
2023-01-16 20:43:09 -0700

    Make multiple little improvements
    Also, improve the publish script a bit, so that it doesn't add a
    discrete commit to the history just for building the HTML.
    
    	modified:   chapters/Handling-the-Frisbee.md
    	modified:   publish_gitbook.sh

--- publish_gitbook.sh
+++ publish_gitbook.sh
@@ -12,8 +12,8 @@ cp -R _book/* .
 # add any new files to the repository index
 git add .
 
-# add all changed files and commit
-git commit -m "Update static website files for GitHub Pages"
+# perform an amendment commit, so as not to clutter history
+git commit --amend
 
-# push to the origin remote
-git push origin master
+# push back up to GitHub
+git push
